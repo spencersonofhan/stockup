728375a006c2ba91ce9a53dd7e8d60b327504061
Spencer Hanson <spencer.hanson@aggiemail.usu.edu>
2020-12-03 09:29:39 -0700

    I just had the greatest match of man vs. software. CMake did not want to play along while I tried to install the 'C++ Curls' library.

--- stock_up.cpp
+++ stock_up.cpp
@@ -1,1 +1,13 @@
 #include "stock_up.hpp"
+
+bigm::stock_up::stock_up()
+{
+    std::cout << "BIGGEST MONEY EVAR!" << std::endl;
+}
+
+std::vector<int> bigm::stock_up::getMeStocks()
+{
+    std::cout << "Yea right" << std::endl;
+    std::vector<int> yr;
+    return yr;
+}
--- stock_up.hpp
+++ stock_up.hpp
@@ -1,5 +1,23 @@
-#include <bits/stdc++.h>
+#pragma once
+
+#include <cpr/cpr.h>
 #include <iostream>
 #include <vector>
 #include <string>
 #include <iomanip>
+#include <fstream>
+
+namespace bigm
+{
+    class stock_up
+    {
+    private:
+        std::vector<int> nums;
+
+    public:
+        stock_up();
+        std::vector<int> getMeStocks();
+
+
+    };
+}
