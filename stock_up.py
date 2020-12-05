ff375bffd1096a3b4ee618ce9ee26a9dd12a2c71
Spencer Hanson <spencer.hanson@aggiemail.usu.edu>
2020-12-05 03:14:12 -0700

    I have achieved exactly what I wanted with Python in 5 minutes. This took me hours in C++

--- stock_up.py
+++ stock_up.py
@@ -1,1 +1,22 @@
-print("Hey it at least runs!")
+import csv
+import time
+import datetime
+
+now = datetime.datetime.now()
+currentDate = str(now.year) + "-" + str(now.month) + "-" + str(now.day)
+print(currentDate)
+
+stocks = []
+header = True
+with open('bought.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        # CSV header
+        if header:
+            header = False
+            continue
+        if row[0] == '':
+            continue
+        stocks.append(row[0])
+
+print(stocks)
