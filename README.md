d991c778b3d32851cad1270ca7250b71af32ffda
Spencer Hanson <spencer.hanson@aggiemail.usu.edu>
2020-12-12 00:29:01 -0700

    API keys are now stored in .config file. Formatted the script to my liking, still a long way to go. I haven't fully decided what to use to graph the financial data.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 STOCKUP IS A WIP :D
 
 Stockup will  be the tool I use to do two things:
-1. Take my C++ knowledge and push it much further while building
+1. Take my C++ knowledge and push it much further while building (I am still
+    going to attempt building this in C++, just not for collecting data)
 2. After completion, it will be a stocks info/visualization program.
--- stock_up.py
+++ stock_up.py
@@ -2,35 +2,63 @@ import csv
 import time
 import datetime
 import requests
+import configparser
 
-now = datetime.datetime.now()
-currentDate = str(now.year) + "-" + str(now.month) + "-" + str(now.day)
-print(currentDate)
+def getUserSymbols():
+    stocks = []
+    header = True
+    with open('bought.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            # CSV header
+            if header:
+                header = False
+                continue
+            if row[0] == '':
+                continue
+            stocks.append(row[0])
 
-stocks = []
-header = True
-with open('bought.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        # CSV header
-        if header:
-            header = False
-            continue
-        if row[0] == '':
-            continue
-        stocks.append(row[0])
+    file.close()
+    return stocks
 
-print(stocks)
+def buildURL():
+    return "https://paper-api.alpaca.markets/v2/orders"
 
-tData = {
-        "symbol": "GOOGL",
-        "qty": 1,
-        "side": "buy",
-        "type": "market",
-        "time_in_force": "day"
-        }
+def getHeaders():
+    config = configparser.ConfigParser()
+    config.read('.config')
 
-# heads
+    headers={
+            "APCA-API-KEY-ID" : config['API']['APCA-API-KEY-ID'],
+            "APCA-API-SECRET-KEY" : config['API']['APCA-API-SECRET-KEY']
+            }
 
-GETReq = requests.post("https://paper-api.alpaca.markets/v2/orders", headers=heads, json=tData)
-print(GETReq.content)
+    return headers
+
+
+
+def main():
+    now = datetime.datetime.now()
+    currentDate = str(now.year) + "-" + str(now.month) + "-" + str(now.day)
+
+    userSymbols = getUserSymbols()
+
+    url = buildURL()
+    heads = getHeaders()
+
+    tData = {
+            "symbol": "GOOGL",
+            "qty": 1,
+            "side": "buy",
+            "type": "market",
+            "time_in_force": "day"
+            }
+
+
+
+
+    GETReq = requests.post(url, headers=heads, json=tData)
+    print(GETReq.content)
+
+
+main()
