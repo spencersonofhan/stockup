4c21ba75cbe01d5d2ee8f5499307a5f7bd3e2aee
Spencer Hanson <spencerbhans@gmail.com>
2021-05-04 03:00:16 -0600

    I've returned to this program. I'm going to add a feature that will look up cryptocurrency prices along with stock prices.

--- stock_up.py
+++ stock_up.py
@@ -3,23 +3,22 @@ import time
 import datetime
 import requests
 import configparser
+import pandas as pd
+import json
+import os
+import sys
 
 def getUserSymbols():
-    stocks = []
-    header = True
-    with open('bought.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            # CSV header
-            if header:
-                header = False
-                continue
-            if row[0] == '':
-                continue
-            stocks.append(row[0])
+    CWD = os.getcwd()
+    File_Name = "symbols.csv"
+    Path = os.path.join(CWD, File_Name)
+    df = pd.read_csv(Path)
 
-    file.close()
-    return stocks
+    user_symbols = []
+    for i in range(df.shape[0]):
+        user_symbols.append(df.iloc[i,0])
+
+    return user_symbols
 
 def buildURL():
     return "https://paper-api.alpaca.markets/v2/orders"
@@ -41,24 +40,49 @@ def main():
     now = datetime.datetime.now()
     currentDate = str(now.year) + "-" + str(now.month) + "-" + str(now.day)
 
-    userSymbols = getUserSymbols()
+    if len(sys.argv) != 2:
+        sys.exit(1)
 
-    url = buildURL()
-    heads = getHeaders()
+    Find_Stocks = True
+    if (sys.argv[1] == "-h"):
+        print("------------------------HELP------------------------")
+        print("FLAGS")
+        print("\t-h: Help")
+        print("\t-s: Look up stock prices")
+        print("\t-c: Look up cryptocurrency prices")
+        sys.exit(0)
+    elif (sys.argv[1] == "-c"):
+        Find_Stocks = False
+    elif (sys.argv[1] == "-s"):
+        pass
+    else:
+        print("\nERR: Include a flag with stock_up.py command!\n")
+        print("FLAGS")
+        print("\t-h: Help")
+        print("\t-s: Look up stock prices")
+        print("\t-c: Look up cryptocurrency prices")
+        sys.exit(1)
+    
+    symbols = getUserSymbols()
+    if Find_Stocks:
+        print(symbols[1])
 
-    tData = {
-            "symbol": "GOOGL",
-            "qty": 1,
-            "side": "buy",
-            "type": "market",
-            "time_in_force": "day"
-            }
-
-
-
-
-    GETReq = requests.post(url, headers=heads, json=tData)
-    print(GETReq.content)
+    # url = buildURL()
+    # heads = getHeaders()
+    #
+    # tData = {
+    #         "symbol": "GOOGL",
+    #         "qty": 1,
+    #         "side": "buy",
+    #         "type": "market",
+    #         "time_in_force": "day"
+    #         }
+    #
+    #
+    #
+    #
+    # GETReq = requests.post(url, headers=heads, json=tData)
+    # print(GETReq.content)
 
 
 main()
